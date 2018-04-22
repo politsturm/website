ede71fe23cff25f989d3306eb104b262fc3d6701
Diadlo <polsha3@gmail.com>
2018-04-23 01:18:49 +0300

    Add social buttons

--- footer.php
+++ footer.php
@@ -15,13 +15,30 @@
 
 <div id="footer">
 	<div class="footer-wrap">
-	
+		<div class="social">
+			<ul>
+				<?php
+					function social($url, $title, $svg_id) {
+						set_query_var("url", $url);
+						set_query_var("title", $title);
+						set_query_var("svg_id", $svg_id);
+						get_template_part('template-parts/social') ;
+					}
+					social("https://www.facebook.com/politsturm/", "Facebook",    "#facebook");
+					social("https://vk.com/politsturm",            "Вконтакте",   "#vk");
+					social("https://twitter.com/politsturm",       "Twitter",     "#twitter");
+					social("http://politshturm.livejournal.com/",  "LiveJournal", "#livejournal");
+					social("https://t.me/politsturm",              "Telegram",    "#telegram");
+				?>
+		  </ul>
+		</div>
+
 		<div class="footer-area"><?php dynamic_sidebar('footer-1'); ?></div>
-		
+
 		<div class="footer-area"><?php dynamic_sidebar('footer-2'); ?></div>
-		
+
 		<div class="footer-area"><?php dynamic_sidebar('footer-3'); ?></div>
-	
+
 	</div>
 </div>
 
---
+++ template-parts/social.php
@@ -0,0 +1,5 @@
+<a href="<?php echo $url ?>" target="_blank" title="<?php echo $title ?>">
+		<span class="svg_icon_container large">
+				<svg><use xlink:href="<?php echo $svg_id ?>"></use></svg>
+		</span>
+</a>
