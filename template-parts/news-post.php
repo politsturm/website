a99604caa90f564560f8f0f1e6103b024afcb2f0
Diadlo <polsha3@gmail.com>
2018-04-27 00:55:13 +0300

    Different time format for mobile

--- template-parts/news-post.php
+++ template-parts/news-post.php
@@ -1,6 +1,7 @@
 <div class="news-post">
 	<div class="news-post-date">
-		<span><?php echo get_the_date('Y-m-d'); ?></span>
+		<span class="news-post-date-mobile"><?php echo get_the_date('m.d'); ?></span>
+		<span class="news-post-date-desktop"><?php echo get_the_date('Y-m-d'); ?></span>
 	</div>
 	<div class="news-post-body">
 		<a href="<?php the_permalink();?>">
