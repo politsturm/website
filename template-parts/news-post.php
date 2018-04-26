6480f3befcdcae4bcce735b321ce81b59d298aed
Diadlo <polsha3@gmail.com>
2018-04-27 00:48:24 +0300

    News for mobile

--- template-parts/news-post.php
+++ template-parts/news-post.php
@@ -1,6 +1,6 @@
 <div class="news-post">
 	<div class="news-post-date">
-		<?php echo get_the_date('Y-m-d'); ?>
+		<span><?php echo get_the_date('Y-m-d'); ?></span>
 	</div>
 	<div class="news-post-body">
 		<a href="<?php the_permalink();?>">
