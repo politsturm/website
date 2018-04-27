fefc724d33f26625926d68b693f28d8ddfc93174
Diadlo <polsha3@gmail.com>
2018-04-27 09:48:29 +0300

    Fix dates

--- template-parts/news-post.php
+++ template-parts/news-post.php
@@ -1,9 +1,9 @@
 <div class="news-post">
-	<div class="news-post-date">
-		<span class="news-post-date-mobile"><?php echo get_the_date('m.d'); ?></span>
-		<span class="news-post-date-desktop"><?php echo get_the_date('Y-m-d'); ?></span>
+	<div class="news-post-date-desktop">
+		<?php echo get_the_date('Y-m-d'); ?>
 	</div>
 	<div class="news-post-body">
+		<span class="news-post-date-mobile"><?php echo get_the_date('m.d'); ?></span>
 		<a href="<?php the_permalink();?>">
 			<?php the_title(); ?>
 		</a>
