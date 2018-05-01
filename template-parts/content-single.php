b45d631d56d3a1ea8c856ae600c3ef50539aea82
Diadlo <polsha3@gmail.com>
2018-05-02 02:43:01 +0300

    Remove trailing spaces

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,18 +11,18 @@
 
 <article itemscope itemtype=http://schema.org/Article id="post-<?php the_ID(); ?>" class="content-post-single">
 <meta itemprop="inLanguage" content="ru" />
-	
-	<div class="content-upmeta-single">		
+
+	<div class="content-upmeta-single">
 		<!-- Начало микроразметки -->
 		<div class="microrazmetka" style="display:none;">
-		
+
 			<?php $iurl = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
 			<span itemprop="headline name"> <?php the_title(); ?></span>
 			<span itemprop="datePublished"> <?php echo get_the_date(Y-M-D); ?></span>
 			<span itemprop="description"> <?php echo get_the_excerpt(); ?> </span>
 			<span itemprop="image"><?php echo $iurl; ?></span>
 			<span style="display:none;" itemprop="keywords"><?php echo get_the_tag_list( '', ', ' ); ?> ,<?php echo get_the_category_list(', '); ?></span>
-								
+
 			<span style="display:none;" itemprop="publisher" itemscope="itemscope" itemtype="http://schema.org/Organization">
 				<span style="display:none;" itemprop="name">politsturm.com - Социалистический информационный ресурс</span>
 				<img src="https://politsturm.com/wp-content/uploads/2017/02/Без-имени-1.png" style="display:none;" itemprop="logo">
@@ -31,33 +31,33 @@
 				<span style="display:none;" itemprop="address">USSR</span>
 				<span style="display:none;" itemprop="telephone">+79192335725</span>
 			</span>
-								
+
 		</div>
 		<!-- Конец микроразметки -->
-		
+
 	</div>
-		
-	<div class="content-summary-single">		
-				
-		<div class="content-meta-card">		
-			
+
+	<div class="content-summary-single">
+
+		<div class="content-meta-card">
+
 			<div itemprop="author" itemscope itemtype="http://schema.org/Person" class="content-author-info-single">
-			
-			<div class="autor-box-left"> 
+
+			<div class="autor-box-left">
 				<div class="author-box-avatar-single"><?php $avatar = get_the_author_meta('basic_user_avatar'); ?> <img class="single-avatar" src="<?php echo $avatar['96']; ?>"></div>
 			</div>
-			
+
 			<div class="autor-box-right">
-				<span class="author-posts-single" itemprop="name"><?php echo get_the_author_meta('display_name'); ?></span><br>			
+				<span class="author-posts-single" itemprop="name"><?php echo get_the_author_meta('display_name'); ?></span><br>
 				<span class="content-date-single" itemprop="datePublished"><?php echo get_the_date(Y-M-D); ?></span>
-			</div>	
-				
 			</div>
-			
-		</div>	
-		
-				
-		
+
+			</div>
+
+		</div>
+
+
+
 		<h1 itemprop="headline name" class="content-title-single">
 			<?php the_title(); ?>
 		</h1>
@@ -65,13 +65,13 @@
 		<div class="content-image-single">
 			<?php the_post_thumbnail('full', array('itemprop' => 'thumbnailUrl', 'alt' => get_the_title() ) ); ?>
 		</div>
-		
+
 		<div itemprop="articleBody">
 		<?php the_content(); ?>
 		</div>
-		
+
 	</div>
-		
+
 </article><!-- #article -->
 
 
