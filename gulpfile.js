097cc85fdfc88371dcc8841271752604dbe3287c
v-prusakov <v-prusakov@yandex-team.ru>
2018-05-19 11:18:27 +0300

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var concat = require('gulp-concat');
+
+
+// default css task
+gulp.task('css', function () {
+    var plugins = [
+        autoprefixer({browsers: ['last 1 version']}),
+        cssnano()
+    ];
+
+    return gulp.src('./assets/css/*.css')
+    	.pipe(concat('style.css'))
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./'));
+});
+
+// watcher
+gulp.task('default', ['css'], function() {
+    gulp.watch(['./assets/css/*.css'], ['default']);
+});
