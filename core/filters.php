34221a8c9b7ec1101543c801260b2a2ae16e719d
Maxim Mironyuk <serieznyi@users.noreply.github.com>
2018-11-24 09:06:46 +0300

    Update filters.php

--- core/filters.php
+++ core/filters.php
@@ -70,9 +70,9 @@ Class POLITSTURM_FILTERS {
 				$not_tags = array();
 			}
 
-            if (($term = get_term_by('slug', $tag_name, 'post_tag')) && !in_array($term->term_id, $not_tags)) {
-                array_push($not_tags, $term->term_id);
-            }
+			if (($term = get_term_by('slug', $tag_name, 'post_tag')) && !in_array($term->term_id, $not_tags)) {
+				array_push($not_tags, $term->term_id);
+			}
 
 			if ($query->is_home() && $query->is_main_query()) {
 				$query->set($field, $not_tags);
