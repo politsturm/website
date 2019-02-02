2a8a804eee8a4993153f4095fd945fd246701a58
Diadlo <polsha3@gmail.com>
2019-02-03 00:03:56 +0300

    Add social-icons.php

---
+++ template-parts/social-icons.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once('render_social_icon.php');
+render_social_icon('https://www.facebook.com/politsturm/', 'Facebook',    '#facebook');
+render_social_icon('https://www.youtube.com/channel/UCdWGGzM6fX5xVq1gDNZev0A', 'Youtube', '#youtube');
+render_social_icon('https://vk.com/politsturm',            'Вконтакте',   '#vk');
+render_social_icon('https://twitter.com/politsturm',       'Twitter',     '#twitter');
+render_social_icon('https://t.me/politsturm',              'Telegram',    '#telegram');
