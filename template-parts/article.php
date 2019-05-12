4723d0c23ac5ed5e16f8f5ed65b38f22a744ed83
v-prusakov <v-prusakov@yandex-team.ru>
2019-05-12 21:42:55 +0300

    Update grid posts

--- template-parts/article.php
+++ template-parts/article.php
@@ -1,4 +1,4 @@
-<a href="<?php the_permalink();?>">
+<a href="<?php the_permalink();?>" class="grid-post__wrapper">
 	<article class="grid-post">
 		<?php
 		if ( in_category( 'video' )) { ?>
@@ -11,14 +11,16 @@
 				$youtube_link = empty($match[1][0]) ? $match[2][0] : $match[1][0];
 			?>
 
-			<iframe width="100%" height="225" class="article__video" src="https://www.youtube.com/embed/<?php echo $youtube_link;?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			<iframe width="100%" height="345" class="article__video" src="https://www.youtube.com/embed/<?php echo $youtube_link;?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
 		<?php } else {
 				$image = get_the_post_thumbnail_url($post->ID, 'medium');
 
 				if ($image) {
 					?>
-					<img src="<?php echo $image;?>" width="100%"  hspace="0" vspace="0" style="display:block;">
+					<div class="main-card__cover" style="background-image: url(<?php echo $image ?>)">
+						<img class="main-card__image" src="<?php echo $image ?>" width="100%"  hspace="0" vspace="0" style="display:block;">
+					</div>
 					<?php
 				}
 
