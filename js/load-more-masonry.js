224af99b22e56f058a87e9a84bdfc11bc3ae725f
Diadlo <polsha3@gmail.com>
2019-06-02 11:44:59 +0300

    Remove masonry usage in load-more script

--- js/load-more-masonry.js
+++ js/load-more-masonry.js
@@ -2,27 +2,16 @@ jQuery(function($){
 	$(document).on('click', '.news-loadmore', function(){
 
 		var button = $(this);
-		var masonryInstance = window.masonryInstance;
 
 		button.text('Загрузка...');
 		loadMore({
 			success: function(data) {
 				button.text('Загрузить ещё');
 				$('.main-news').append(data);
-				var imageCount = $(data).find('img').length;
-
-				var imagesLoaded = 0;
-				$('.main-news img').load(function () {
-					imagesLoaded++;
-					if (imagesLoaded === imageCount) {
-						masonryInstance.reloadItems();
-						masonryInstance.layout();
-					}
-				})
 			},
 			end: function() {
 				button.remove();
-            }
+			}
 		});
 	});
 });
