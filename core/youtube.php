4deff9a84fa6ac3fa86dc2f08dccf426e22dd1bc
Diadlo <polsha3@gmail.com>
2020-02-16 21:48:55 +0300

    Parse http youtube links

--- core/youtube.php
+++ core/youtube.php
@@ -3,9 +3,9 @@
 function get_youtube_code($content)
 {
 	$youtube_re_list = array(
-		'https.*\?v\=(.[_0-9a-zA-Z]+)',
-		'https.*youtu\.be\/(.[_0-9a-zA-Z\-]+)',
-		'https.*youtube.com\/embed\/(.[_0-9a-zA-Z\-]+)',
+		'http.*\?v\=(.[_0-9a-zA-Z]+)',
+		'http.*youtu\.be\/(.[_0-9a-zA-Z\-]+)',
+		'http.*youtube.com\/embed\/(.[_0-9a-zA-Z\-]+)',
 	);
 	$reg_exp = '/'.implode('|', $youtube_re_list).'/i';
 	preg_match_all($reg_exp, $content, $match);
